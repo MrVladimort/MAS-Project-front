2cae937fd7824c2161e7c0b24b4c42ba06781bd7
mrvladimort <mrvladimort@gmail.com>
2019-06-09 20:43:01 +0200

    heroku urls

--- src/configs/main.js
+++ src/configs/main.js
@@ -1,6 +1,6 @@
 export default {
     // apiHost: "http://127.0.0.1:8080/api",
     // clientHost: "http://127.0.0.1:3000",
-    apiHost: "https://ipb-project-app-back.herokuapp.com/api",
-    clientHost: "https://ipb-project-app-front.herokuapp.com",
+    apiHost: "https://mas-project-app-back.herokuapp.com/api",
+    clientHost: "https://mas-project-app-front.herokuapp.com",
 }
